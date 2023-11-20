bcf0c42f47f451aaa2dd1bbead520ebb037e4774
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-11-20 20:17:41 +0100

    Exercise nr 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Exercise1 from "./Exercises/Exercise1";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Typescript is cool 😎</h1>
+      <Exercise1 />
     </>
   );
 }
