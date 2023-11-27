80e5229e39829581add342b30b69f29d3ad90f86
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2023-11-27 20:17:31 +0100

    exercise 3  & exercise 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Exercise1 from "./Exercises/Exercise1";
 import Exercise2 from "./Exercises/Exercise2";
+import Exercise3 from "./Exercises/Exercise3";
+import Exercise4 from "./Exercises/Exercise4";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <h1>Typescript is cool 😎</h1>
       <Exercise1 />
       <Exercise2 />
+      <Exercise3 />
+      <Exercise4 />
     </>
   );
 }
